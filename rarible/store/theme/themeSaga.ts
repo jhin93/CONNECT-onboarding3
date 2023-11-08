24059f8b8bac63e732dd826f2d00c3fb3055461a
jhin93 <scene1993@gmail.com>
2023-11-08 16:42:15 +0900

    saga 파일 수정

--- rarible/store/theme/themeSaga.ts
+++ rarible/store/theme/themeSaga.ts
@@ -2,10 +2,13 @@
 import {put, takeLatest} from "@redux-saga/core/effects";
 import { toggleDarkMode } from "./themeSlice";
 
+// Saga의 시작 지점
 function* toggleDarkModeSaga () {
-    yield put(toggleDarkMode());
+    yield put(toggleDarkMode()); // 'toggleDarkMode' 액션을 디스패치하여 다크 모드를 토글
 }
 
+// 'toggleDarkModeSaga' 함수를 호출하는 사가 워쳐
 export function* watchThemeSagas() {
-    yield takeLatest('THEME/TOGGLE_DARK_MODE', toggleDarkModeSaga);
+    // THEME/TOGGLE_DARK_MODE' 액션을 감시하고, 해당 액션이 발생하면 `toggleDarkModeSaga` 함수를 호출
+    yield toggleDarkModeSaga;
 }
