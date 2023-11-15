1284ffb8d4d74fcef4c1f019f4d771164695d0d9
jhin93 <scene1993@gmail.com>
2023-11-15 16:15:00 +0900

    Cannot add property ..., object is not extensible - 주석추가

--- rarible/feature/getItem.ts
+++ rarible/feature/getItem.ts
@@ -6,10 +6,10 @@ const getItems = async () => {
     const localURL = "http://localhost:3000";
     try {
         const rawData = await axios.get(localURL + '/api/itemListFetch');
+        // 1. .map() 메소드를 사용하여 새로운 배열을 생성하고, 이 배열을 itemMetadataArr에 할당(.push 사용 X)
+        // 2. rawDataList와 itemMetadataArr를 함수 내부로 옮겨서 함수 외부에서 발생할 수 있는 문제를 방지
         const rawDataList: rawMetadata[] = rawData.data.listings;
-
         const itemMetadataArr: ItemMetadata[] = rawDataList.map((item) => item.metadata);
-
         return itemMetadataArr;
     } catch (error) {
         console.error("Error fetching data from getItems.ts:", error);
