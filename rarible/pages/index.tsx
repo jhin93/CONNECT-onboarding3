b372562d44b019f726de29c82730fd5bed9841ca
jhin93 <scene1993@gmail.com>
2023-11-15 15:44:17 +0900

    카운터, 테마 전환 기능 배제

--- rarible/pages/index.tsx
+++ rarible/pages/index.tsx
@@ -4,12 +4,8 @@ import { RootState } from '../store/store';
 import { useEffect } from "react";
 import ItemCard from "../components/ItemCard";
 
-type ActionType = 'increment' | 'decrement' | 'reset'
-
 function Home() {
     const dispatch = useDispatch();
-    const count = useSelector((state: RootState) => state.counter);
-    const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode)
     const itemsArr = useSelector((state: RootState) => state.item.items);
     const loading = useSelector((state: RootState) => state.item.loading);
 
@@ -21,36 +17,9 @@ function Home() {
         console.log('(index.tsx)Items have been updated:', itemsArr);
     }, [itemsArr]); // 아이템 리스트에 변경이 있을 때 실행
 
-    const toggleTheme = () => {
-        dispatch({ type: 'THEME/TOGGLE_DARK_MODE_ASYNC' });
-    };
-
-    const handleButtonClick = (action: ActionType) => () => {
-        switch (action) {
-            case 'increment':
-                dispatch({ type: 'COUNTER/INCREMENT' });
-                break;
-            case 'decrement':
-                dispatch({ type: 'COUNTER/DECREMENT' });
-                break;
-            case 'reset':
-                dispatch({ type: 'COUNTER/RESET' });
-                break;
-            default:
-                break;
-        }
-    };
 
     return (
-        <div className={isDarkMode ? 'dark' : 'light'}>
-            <div>
-                <h1>Counting</h1>
-                <p>Count: {count}</p>
-                <button onClick={handleButtonClick('increment')}>Increment</button>
-                <button onClick={handleButtonClick('decrement')}>Decrement</button>
-                <button onClick={handleButtonClick('reset')}>Reset</button>
-                <button onClick={toggleTheme}>Toggle Dark Mode</button>
-            </div>
+        <div>
             <div>
                 {loading ? (
                     <div>Loading...</div>
--- rarible/store/store.ts
+++ rarible/store/store.ts
@@ -3,11 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
-import counterReducer from './counter/counterSlice';
-import { watchCounterSagas } from "./counter/counterSaga";
-import themeReducer from './theme/themeSlice';
-import { watchThemeSagas } from "./theme/themeSaga";
-
 import itemReducer from './fetch/itemSlice'; // 추가된 import
 import { watchItemSagas } from './fetch/itemSaga'; // 추가된 import
 
@@ -18,14 +13,12 @@ export type RootState = ReturnType<typeof rootReducer>;
 function* rootSaga() {
     // yield : 제어를 호출자에게 반환하고 함수의 상태를 일시 중지하는 데 사용. 주로 비동기 작업을 수행하고 그 결과를 기다리는 데 사용
     // all : 여러 Saga 이펙트를 배열로 전달하면 all은 이들을 동시에 시작하고 모든 작업이 완료될 때까지 기다립니다. 즉, 병렬 실행을 지원합니다.
-    yield all([watchCounterSagas(), watchThemeSagas(), watchItemSagas()]);
+    yield all([watchItemSagas()]);
 }
 
 const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer = combineReducers({
-    counter: counterReducer,
-    theme: themeReducer,
     item: itemReducer
 });
 
