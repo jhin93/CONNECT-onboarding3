65a974d57e3ae1ef528d755845a41141e8e17c61
jhin93 <scene1993@gmail.com>
2023-11-16 14:26:24 +0900

    검색기능 완료

--- rarible/pages/index.tsx
+++ rarible/pages/index.tsx
@@ -2,16 +2,16 @@
 
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store/store';
-import { useEffect, useState } from "react"; // useState 추가
+import { useEffect, useState } from "react";
 import ItemCard from "../components/ItemCard";
-import {setSearchResult} from "../store/fetch/itemSlice";
+import { setSearchResult } from '../store/fetch/itemSlice';
 
 function Home() {
     const dispatch = useDispatch();
     const itemsArr = useSelector((state: RootState) => state.item.items);
     const loading = useSelector((state: RootState) => state.item.loading);
-    const [searchTerm, setSearchTerm] = useState(""); // 검색어 상태 추가
-    const searchResult = useSelector((state: RootState) => state.item.searchResult); // 검색 결과 상태 추가
+    const [searchTerm, setSearchTerm] = useState("");
+    const searchResult = useSelector((state: RootState) => state.item.searchResult);
 
     useEffect(() => {
         dispatch({type: 'ITEM/FETCH_ITEMS_ASYNC'});
@@ -19,18 +19,25 @@ function Home() {
 
     useEffect(() => {
         console.log('(index.tsx)Items have been updated:', itemsArr);
-    }, [itemsArr]); // 아이템 리스트에 변경이 있을 때 실행
+    }, [itemsArr]);
 
-    useEffect(() => {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            // Enter 키를 눌렀을 때만 검색을 수행합니다.
+            performSearch(e.currentTarget.value);
+        }
+    };
+
+    const performSearch = (searchTerm: string) => {
+        // 검색어가 빈 문자열인 경우 모든 아이템을 검색 결과로 사용
         if (searchTerm === "") {
-            // 검색어가 빈 문자열인 경우 모든 아이템을 검색 결과로 사용
             dispatch(setSearchResult(itemsArr));
         } else {
             // 정확한 일치 비교
             const filteredItems = itemsArr.filter(item => item.name === searchTerm);
             dispatch(setSearchResult(filteredItems));
         }
-    }, [searchTerm, itemsArr]);
+    };
 
     return (
         <div>
@@ -39,7 +46,8 @@ function Home() {
                     type="text"
                     placeholder="Search by item name"
                     value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)} // 검색어 입력 시 검색어 상태 업데이트
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={handleKeyDown} // Enter 키 입력 감지
                 />
                 {loading ? (
                     <div>Loading...</div>
