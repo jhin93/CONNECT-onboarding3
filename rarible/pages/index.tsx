d8418df3f5562947c53941d7710819bbd54a18f5
jhin93 <scene1993@gmail.com>
2023-11-18 17:11:16 +0900

    4-5열 전환 버튼 구현

--- rarible/pages/index.tsx
+++ rarible/pages/index.tsx
@@ -13,7 +13,7 @@ const HomeContainer = styled.div`
 
 const ItemGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr); 
+  grid-template-columns: repeat(4, 1fr);
   gap: 2vw;
   border-radius: 8px;
 `;
@@ -26,26 +26,31 @@ const SearchInput = styled.input`
   border-radius: 8px;
 `;
 
+const NavBar = styled.div`
+  display: flex;
+`
+
 function Home() {
     const dispatch = useDispatch();
     const itemsArr = useSelector((state: RootState) => state.item.items);
     const loading = useSelector((state: RootState) => state.item.loading);
     const [searchTerm, setSearchTerm] = useState("");
     const searchResult = useSelector((state: RootState) => state.item.searchResult);
+    const [gridColumns, setGridColumns] = useState(4); // 초기 열 수를 4로 설정
 
     useEffect(() => {
         dispatch({ type: 'ITEM/FETCH_ITEMS_ASYNC' });
     }, []);
 
     useEffect(() => {
-        console.log('(index.tsx)Items have been updated:', itemsArr);
+        console.log('(index.tsx) Items have been updated:', itemsArr);
         setSearchTerm("");
-        dispatch(setSearchResult(itemsArr)); // 페이지 최초 렌더링 시에도 모든 아이템이 나타나도록 초기 검색어를 빈 문자열로 설정
+        dispatch(setSearchResult(itemsArr));
     }, [itemsArr]);
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-            performSearch(e.currentTarget.value); // Enter 키 입력시 검색 실행
+            performSearch(e.currentTarget.value);
         }
     };
 
@@ -54,28 +59,34 @@ function Home() {
         dispatch(setSearchResult(filteredItems));
     };
 
+    const handleGridChange = (newColumns: number) => {
+        setGridColumns(newColumns);
+    };
+
     return (
         <HomeContainer>
-            <div>
+            <NavBar>
                 <SearchInput
                     type="text"
                     placeholder="Search with item name"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
-                    onKeyDown={handleKeyDown} // Enter 키 입력 감지
+                    onKeyDown={handleKeyDown}
                 />
-            </div>
-                {loading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <ItemGrid>
-                        {searchResult.length > 0 ? (
-                            searchResult.map((item) => <ItemCard key={item.id} item={item} />)
-                        ) : (
-                            <div>No items available</div>
-                        )}
-                    </ItemGrid>
-                )}
+                <button onClick={() => handleGridChange(4)}>4 열</button>
+                <button onClick={() => handleGridChange(5)}>5 열</button>
+            </NavBar>
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <ItemGrid style={{ gridTemplateColumns: `repeat(${gridColumns}, 1fr)` }}>
+                    {searchResult.length > 0 ? (
+                        searchResult.map((item) => <ItemCard key={item.id} item={item} />)
+                    ) : (
+                        <div>No items available</div>
+                    )}
+                </ItemGrid>
+            )}
         </HomeContainer>
     );
 }
